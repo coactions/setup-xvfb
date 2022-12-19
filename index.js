b70ee9cdde80f87405a34595ded00afbed07d6e0
Sorin Sbarnea <sorin.sbarnea@gmail.com>
2022-12-19 14:40:31 +0000

    Add missing x11-xserver-utils (#5)

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const exec = require("@actions/exec");
 async function main() {
   try {
     if (process.platform == "linux") {
-      await exec.exec("sudo apt-get install -y xvfb");
+      await exec.exec("sudo apt-get install -y xvfb x11-xserver-utils");
     }
 
     const commands = core.getInput("run", { required: true }).split("\n");
